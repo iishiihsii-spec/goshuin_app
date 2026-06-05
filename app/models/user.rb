cf65d2613ea34de2df4fdb5f2b71730e64093221
ko <iishiihsii@gmail.com>
2026-06-05 14:49:32 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # バリデーション
+  validates :nickname, presence: true
+
+  # アソシエーション
+  # has_many :goshuins
+  # has_many :user_stamps
+  # has_many :stamps, through: :user_stamps
+
 end
