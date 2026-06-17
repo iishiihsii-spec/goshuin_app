7b3e3b7834acf6afe138c36e365ddf7247a48dab
ko <iishiihsii@gmail.com>
2026-06-17 15:01:27 +0900

    詳細ページと編集ページにマップ表示機能を追加

--- app/javascript/goshuin_map.js
+++ app/javascript/goshuin_map.js
@@ -18,9 +18,6 @@ const goshuinMap = () => {
     const lat = e.latlng.lat;
     const lng = e.latlng.lng;
 
-    console.log(lat);
-    console.log(lng);
-
     latitudeField.value = lat;
     longitudeField.value = lng;
 
@@ -35,4 +32,64 @@ const goshuinMap = () => {
   });
 };
 
+// 詳細機能
+const goshuinShowMap = () => {
+  const showMapElement = document.getElementById("goshuin-show-map");
+  if (!showMapElement) return
+  const latitude = showMapElement.dataset.latitude;
+  const longitude = showMapElement.dataset.longitude;
+  const placeName = showMapElement.dataset.placeName;
+
+  const map = L.map(showMapElement).setView([latitude, longitude], 13);
+
+   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors"
+  }).addTo(map);
+
+  L.marker([latitude, longitude]).addTo(map).bindPopup(placeName).openPopup();
+
+};
+
+// 編集機能 
+const goshuinEditMap = () => {
+  const editMapElement = document.getElementById("goshuin-edit-map");
+  if (!editMapElement) return
+  const latitude = editMapElement.dataset.latitude
+  const longitude = editMapElement.dataset.longitude
+  const placeName = editMapElement.dataset.placeName
+
+  const latitudeField = document.getElementById("goshuin-latitude")
+  const longitudeField = document.getElementById("goshuin-longitude")
+
+  let marker;
+  let map;
+  if (latitude && longitude) {
+    map = L.map(editMapElement).setView([latitude, longitude], 13);
+    marker = L.marker([latitude, longitude]).addTo(map).bindPopup(placeName).openPopup();
+  } else { 
+    map = L.map(editMapElement).setView([35.681236, 139.767125], 13);
+  }
+  
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap contributors"
+  }).addTo(map);
+  
+
+  map.on("click", (e) => {
+    const lat = e.latlng.lat;
+    const lng = e.latlng.lng;
+
+    latitudeField.value = lat;
+    longitudeField.value = lng;
+
+    if (marker) {
+      marker.setLatLng(e.latlng);
+    } else {
+      marker = L.marker(e.latlng).addTo(map);
+    }
+  });
+};
+
 window.addEventListener("turbo:load", goshuinMap);
+window.addEventListener("turbo:load", goshuinShowMap);
+window.addEventListener("turbo:load", goshuinEditMap)
