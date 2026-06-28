ce2f75f873f253523d1451ff09048bd3c4664fdb
ko <iishiihsii@gmail.com>
2026-06-28 16:54:32 +0900

    レアリティの表示

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -1,5 +1,16 @@
 class Stamp < ApplicationRecord
   has_many :user_stamps
   has_many :users, through: :user_stamps
-  
+  def rarity_name
+    case rarity_id
+    when 1
+      "Normal"
+    when 2
+      "Rare"
+    when 3
+      "Super Rare"
+    else
+      "Ultra Rare"
+    end
+  end
 end
